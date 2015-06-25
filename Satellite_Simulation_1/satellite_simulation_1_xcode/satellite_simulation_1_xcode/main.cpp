890cd0f98c67866d9d7c091d9f9b1b35b037b8d7
dj9889 <daj17@stanford.edu>
2015-06-25 16:50:55 -0700

    found a bug in the definition of alpha; fixed it

--- Satellite_Simulation_1/satellite_simulation_1_xcode/satellite_simulation_1_xcode/main.cpp
+++ Satellite_Simulation_1/satellite_simulation_1_xcode/satellite_simulation_1_xcode/main.cpp
@@ -26,15 +26,15 @@ using namespace std;
 //Function headers
 
 double returnDistanceBetweenPoints(double X, double Y, double Z, double X0, double Y0, double Z0);
-
+double returnAngleBetweenTwoVectors(double X, double Y, double Z, double X0, double Y0, double Z0);
 //THESE VARIABLES ARE SET IN MAIN...///
 double SUN_X;
 double SUN_Y;
 double SUN_Z;
 
-double SAT_X_POSITION;
-double SAT_Y_POSITION;
-double SAT_Z_POSITION;
+double SAT_X;
+double SAT_Y;
+double SAT_Z;
 
 double albedo;
 ///////////////////////////////////////
@@ -55,15 +55,15 @@ const double M_b=E_s/4; //As derived in the paper.
 
 const int NUM_STEPS_THETA=20;
 
-const int NUM_STEPS_ALPHA=20;
+const int NUM_STEPS_BETA=20;
 
-const double THETA_MAX=2*M_PI;
+double THETA_MAX;
 
-const double ALPHA_MAX=acos(EARTH_RADIUS/returnDistanceBetweenPoints(0, 0, 0, SAT_X_POSITION, SAT_Y_POSITION, SAT_Z_POSITION));
+double BETA_MAX;
 
-const double theta_interval=THETA_MAX/NUM_STEPS_THETA;
+double theta_interval;
 
-const double alpha_interval=ALPHA_MAX/NUM_STEPS_ALPHA;
+double BETA_interval;
 
 /////////////////////////////////////////////////////END CONSTANT DEFINITIONS
 //////////////////////////////////////////////////////////////////////////
@@ -73,25 +73,32 @@ double magnitudeOfVector (double x, double y, double z)  {
     return pow( (pow(x,2) + pow(y,2) + pow(z,2)) ,  .5);
 }
 
-double returnSunAngleForAreaElementAt(double alpha, double theta)  { //this is the angle between the sun and the normal to the earth element.
-    double x=EARTH_RADIUS*cos(theta)*sin(alpha); //just translating from cartesian to spherical.
-    double y=EARTH_RADIUS*sin(theta)*sin(alpha);
-    double z=EARTH_RADIUS*cos(alpha);
-    
-    //use dot product
-    double denom=magnitudeOfVector(x, y, z)*magnitudeOfVector(SUN_X, SUN_Y, SUN_Z);
-    double angle=acos((x*SUN_X+y*SUN_Y+z*SUN_Z)/denom);
-    return angle;
+double returnSunAngleForAreaElementAt(double BETA, double theta)  { //this is the angle between the sun and the normal vector to the earth element.
+    double x=EARTH_RADIUS*cos(theta)*sin(BETA); //just translating from cartesian to spherical.
+    double y=EARTH_RADIUS*sin(theta)*sin(BETA);
+    double z=EARTH_RADIUS*cos(BETA);
+    return returnAngleBetweenTwoVectors(x, y, z, SUN_X, SUN_Y, SUN_Z);
 }
 
-double calculateSunlightEffect(double alpha, double theta) { //related math in djax notes...
-    double x=EARTH_RADIUS*cos(theta)*sin(alpha); //just translating from cartesian to spherical.
-    double y=EARTH_RADIUS*sin(theta)*sin(alpha);
-    double z=EARTH_RADIUS*cos(alpha);
+double calculateSunlightEffect(double BETA, double theta) { //related math in djax notes...
+    double x=EARTH_RADIUS*cos(theta)*sin(BETA); //just translating from cartesian to spherical.
+    double y=EARTH_RADIUS*sin(theta)*sin(BETA);
+    double z=EARTH_RADIUS*cos(BETA);
     
-    double r = returnDistanceBetweenPoints(x, y, z, SAT_X_POSITION, SAT_Y_POSITION, SAT_Z_POSITION);  //distance between area element and the satellite.
+    double r = returnDistanceBetweenPoints(x, y, z, SAT_X, SAT_Y, SAT_Z);  //distance between area element and the satellite.
     
-    return (albedo*E_s*cos(returnSunAngleForAreaElementAt(alpha,theta))+e*M_b)*Ac/(M_PI*pow(r,2))*cos(alpha)*theta_interval*alpha_interval; //this is an approximation for our area, using dA=d_alpha*d_theta. This probably introduces a slight error (as opposed to a flat integral, an integral over a curved surface mutates your dA).
+    //We have been using the variable BETA to denote the limits of our discretization.
+    //Note that alpha is used the paper to describe the angle between the normal vector to the area element and the satellite vector. This is different than the BETA we have been using an an for loop variable.
+    double alpha=returnAngleBetweenTwoVectors(x, y, z, SAT_X, SAT_Y, SAT_Z);
+    
+    return (albedo*E_s*cos(returnSunAngleForAreaElementAt(BETA,theta))+e*M_b)*Ac/(M_PI*pow(r,2))*cos(alpha)*theta_interval*BETA_interval; //this is an approximation for our area, using dA=d_BETA*d_theta. This probably introduces a slight error (as opposed to a flat integral, an integral over a curved surface mutates your dA).
+}
+
+double returnAngleBetweenTwoVectors(double X, double Y, double Z, double X0, double Y0, double Z0) {
+    double MAG_1=pow( (pow(X,2) + pow(Y,2)  + pow(Z,2)) ,  .5);
+    double MAG_2=pow( (pow(X0,2) + pow(Y0,2)  + pow(Z0,2)) ,  .5);
+    double DOT_PROD=X*X0+Y*Y0+Z*Z0;
+    return MAG_1*MAG_2/DOT_PROD;
 }
 
 double returnDistanceBetweenPoints(double X, double Y, double Z, double X0, double Y0, double Z0)    {
@@ -100,11 +107,11 @@ double returnDistanceBetweenPoints(double X, double Y, double Z, double X0, doub
     return distance;
 }
 
-bool returnValidPointInRelationToSun (double alpha, double theta) //returns whether a point
+bool returnValidPointInRelationToSun (double BETA, double theta) //returns whether a point
 {
-    double x=EARTH_RADIUS*cos(theta)*sin(alpha); //just translating from cartesian to spherical.
-    double y=EARTH_RADIUS*sin(theta)*sin(alpha);
-    double z=EARTH_RADIUS*cos(alpha);
+    double x=EARTH_RADIUS*cos(theta)*sin(BETA); //just translating from cartesian to spherical.
+    double y=EARTH_RADIUS*sin(theta)*sin(BETA);
+    double z=EARTH_RADIUS*cos(BETA);
     
     double ACTUAL_DISTANCE=returnDistanceBetweenPoints(x, y, z, SUN_X, SUN_Y, SUN_Z);
     
@@ -114,36 +121,45 @@ bool returnValidPointInRelationToSun (double alpha, double theta) //returns whet
 double returnFluxForParameters(double SAT_X_PARAM, double SAT_Y_PARAM, double SAT_Z_PARAM,double SUN_X_PARAM, double SUN_Y_PARAM, double SUN_Z_PARAM, double albedo_PARAM)    { //function that simply iterates over a square, and calculates sunlight effect accordinly...
     
     //asign parameters as global variables.
-    SAT_X_POSITION=SAT_X_PARAM;
-    SAT_Y_POSITION=SAT_Y_PARAM;
-    SAT_Z_POSITION=SAT_Z_PARAM;
+    SAT_X=SAT_X_PARAM;
+    SAT_Y=SAT_Y_PARAM;
+    SAT_Z=SAT_Z_PARAM;
     SUN_X=SUN_X_PARAM;
     SUN_Y=SUN_Y_PARAM;
     SUN_Z=SUN_Z_PARAM;
     albedo=albedo_PARAM;
     
-    double SUN_POSITION_ABOVE_EARTH=returnDistanceBetweenPoints(0, 0, 0, SUN_X, SUN_Y, SUN_Z);
+    //We use the imput params to determine a few other global constants.
+    THETA_MAX=2*M_PI;
     
-    double MAX_ANGLE=acos(EARTH_RADIUS/SUN_POSITION_ABOVE_EARTH);
+    BETA_MAX=acos(EARTH_RADIUS/returnDistanceBetweenPoints(0, 0, 0, SAT_X, SAT_Y, SAT_Z));
     
-    //defined in diagram, different than alpha
-    MAX_ACCEPTABLE_DISTANCE=sin(MAX_ANGLE)*SUN_POSITION_ABOVE_EARTH;
+    theta_interval=THETA_MAX/NUM_STEPS_THETA;
+    
+    BETA_interval=BETA_MAX/NUM_STEPS_BETA;
+    
+    double SUN_POSITION_FROM_EARTH_CENTER=returnDistanceBetweenPoints(0, 0, 0, SUN_X, SUN_Y, SUN_Z);
+    
+    double MAX_ANGLE=acos(EARTH_RADIUS/SUN_POSITION_FROM_EARTH_CENTER);
+    
+    //defined in diagram, different than BETA
+    MAX_ACCEPTABLE_DISTANCE=sin(MAX_ANGLE)*SUN_POSITION_FROM_EARTH_CENTER;
     
     double NET_FLUX=0;
     
-    for(double alpha = 0; alpha < ALPHA_MAX; alpha = alpha + alpha_interval)
+    for(double BETA = 0; BETA < BETA_MAX; BETA = BETA + BETA_interval)
     {
         for(double theta = 0; theta <THETA_MAX; theta = theta + theta_interval)
         {
-            double alpha_new=alpha+alpha_interval/2; //approximating the centroid.
+            double BETA_new=BETA+BETA_interval/2; //approximating the centroid.
             double theta_new=theta+theta_interval/2;
             
-            if (returnValidPointInRelationToSun(alpha_new,theta_new))  {
+            if (returnValidPointInRelationToSun(BETA_new,theta_new))  {
                 //calculate the z position for this discrete piece
                 
                 double dFLUX;
                 
-                dFLUX=calculateSunlightEffect(alpha,theta);
+                dFLUX=calculateSunlightEffect(BETA,theta);
                 
                 NET_FLUX=NET_FLUX+dFLUX;
             }
@@ -166,33 +182,33 @@ int main ()
     double result= returnFluxForParameters(SAT_X_PARAM, SAT_Y_PARAM, SAT_Z_PARAM, SUN_X_PARAM, SUN_Y_PARAM, SUN_Z_PARAM, albedo_PARAM);
     cout << result << endl;
     
-//    //ToCheck the above function, let's run a for loop that simulates if the earth and satellite didn't move but the sun rose and set...
-//    
-//    //    //let's iterate through some values of the sun to check ...
-//    //
-//    double x_initial=150*pow(10,9); //sun starts out on the horizon.
-//    double x_final=-150*pow(10,9);
-//    int NUM_STEPS=20;
-//    double sun_interval=fabs((x_final-x_initial)/NUM_STEPS);
-//    
-//    ofstream myfile;
-//    myfile.open("outPutFile", std::ofstream::out | std::ofstream::trunc); //open and delete the previous contents of the file from past simulation runs...
-//    if (!myfile.is_open()) {
-//        cerr << "Fail to open output file\n";
-//        exit(EXIT_FAILURE);
-//    }
-//    
-//    for(double SUN_X = x_initial; SUN_X > x_final; SUN_X = SUN_X - sun_interval)
-//    {
-//        //we're not varying y, so we don't need to worry about that
-//        //we have to calculate our z value.
-//        double Z_Pos=pow( pow(150*pow(10,9),2) - pow(SUN_X,2) , .5 );
-//        double result=returnFluxForParameters(SAT_X_PARAM, SAT_Y_PARAM, SAT_Z_PARAM, SUN_X, 0, Z_Pos, albedo_PARAM);
-//        
-//        myfile << result;
-//        myfile << "\n";
-//        myfile.close();
-   // }
+    //    //ToCheck the above function, let's run a for loop that simulates if the earth and satellite didn't move but the sun rose and set...
+    //
+    //    //    //let's iterate through some values of the sun to check ...
+    //    //
+    //    double x_initial=150*pow(10,9); //sun starts out on the horizon.
+    //    double x_final=-150*pow(10,9);
+    //    int NUM_STEPS=20;
+    //    double sun_interval=fabs((x_final-x_initial)/NUM_STEPS);
+    //
+    //    ofstream myfile;
+    //    myfile.open("outPutFile", std::ofstream::out | std::ofstream::trunc); //open and delete the previous contents of the file from past simulation runs...
+    //    if (!myfile.is_open()) {
+    //        cerr << "Fail to open output file\n";
+    //        exit(EXIT_FAILURE);
+    //    }
+    //
+    //    for(double SUN_X = x_initial; SUN_X > x_final; SUN_X = SUN_X - sun_interval)
+    //    {
+    //        //we're not varying y, so we don't need to worry about that
+    //        //we have to calculate our z value.
+    //        double Z_Pos=pow( pow(150*pow(10,9),2) - pow(SUN_X,2) , .5 );
+    //        double result=returnFluxForParameters(SAT_X_PARAM, SAT_Y_PARAM, SAT_Z_PARAM, SUN_X, 0, Z_Pos, albedo_PARAM);
+    //
+    //        myfile << result;
+    //        myfile << "\n";
+    //        myfile.close();
+    // }
     
     //    double answer = runForLoop();
     //    cout << "Result: " << answer;
