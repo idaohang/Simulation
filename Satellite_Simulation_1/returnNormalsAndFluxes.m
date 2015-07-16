ea2ec51270091a50d39dd5fac9267d9600900c19
dj9889 <daj17@stanford.edu>
2015-07-16 15:03:07 -0700

    moving along; working on optimization to move time from N^2 to N

--- Satellite_Simulation_1/returnNormalsAndFluxes.m
+++ Satellite_Simulation_1/returnNormalsAndFluxes.m
@@ -2,7 +2,7 @@
 %going on in the C++ flux calculator.
 %Made by David Jackson Summer 2015
 function [unitVectMatrix,dFluxMatrix]=returnNormalsAndFluxes(SAT_VECT,SUN_VECT,albedo)
-
+tic;
 EARTH_RADIUS=6371000;
 E_s=1360;
 e=1-albedo;
@@ -41,48 +41,48 @@ unitVectMatrix=zeros(maxCounterValue,3);
 
 counter=0;
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Between these comments is a test of a smarter algorithm that should get
-%the same answer
-%Looks for closest point and then calculates from there...walking around
-%both ways...
-
-%First create a sun copy point that has z=0; use an alpha in range [>0 :
-%<pi/2] to determine theta sun...
-
-SUN_COPY_X=SUN_X;
-SUN_COPY_Y=SUN_Y;
-SUN_COPY_Z=0;
-
-A=[SUN_COPY_X,SUN_COPY_Y,SUN_COPY_Z];
-B=[0,1,0];
-
-angleBetweenSunAndThetaZero=acos(dot(A,B)/(norm(A)*norm(B)));
-
-%Find closest theta and then determine if you must round up or down...
-
-remainderAngle=angleBetweenSunAndThetaZero-mod(angleBetweenSunAndThetaZero,INTERVAL_THETA);
-
-if (remainderAngle==0)
-    START_THETA=angleBetweenSunAndThetaZero;
-elseif (remainderAngle-INTERVAL_THETA>0) % round up...
-    START_THETA=angleBetweenSunAndThetaZero+(INTERVAL_THETA-remainderAngle);
-else %round down...
-    START_THETA=angleBetweenSunAndThetaZero-remainderAngle;
-end
-
-%Now that we've identified our START_THETA, we're ready to iterate...
-
-SUN_VECTOR=[SUN_X SUN_Y SUN_Z];
-SAT_VECTOR=[SAT_X SAT_Y SAT_Z];
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %Between these comments is a test of a smarter algorithm that should get
+% %the same answer
+% %Looks for closest point and then calculates from there...walking around
+% %both ways...
+% 
+% %First create a sun copy point that has z=0; use an alpha in range [>0 :
+% %<pi/2] to determine theta sun...
+% 
+% SUN_COPY_X=SUN_X;
+% SUN_COPY_Y=SUN_Y;
+% SUN_COPY_Z=0;
+% 
+% A=[SUN_COPY_X,SUN_COPY_Y,SUN_COPY_Z];
+% B=[0,1,0];
+% 
+% angleBetweenSunAndThetaZero=acos(dot(A,B)/(norm(A)*norm(B)));
+% 
+% %Find closest theta and then determine if you must round up or down...
+% 
+% remainderAngle=angleBetweenSunAndThetaZero-mod(angleBetweenSunAndThetaZero,INTERVAL_THETA);
+% 
+% if (remainderAngle==0)
+%     START_THETA=angleBetweenSunAndThetaZero;
+% elseif (remainderAngle-INTERVAL_THETA>0) % round up...
+%     START_THETA=angleBetweenSunAndThetaZero+(INTERVAL_THETA-remainderAngle);
+% else %round down...
+%     START_THETA=angleBetweenSunAndThetaZero-remainderAngle;
+% end
+% 
+% %Now that we've identified our START_THETA, we're ready to iterate...
+% 
+% SUN_VECTOR=[SUN_X SUN_Y SUN_Z];
+% SAT_VECTOR=[SAT_X SAT_Y SAT_Z];
 
 % I think an angle of theta=0 corresponds to unit vector [0 1 0] //TODO CHECK THIS
 
@@ -95,12 +95,15 @@ SAT_VECTOR=[SAT_X SAT_Y SAT_Z];
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-for alpha=INTERVAL_ALPHA/2:INTERVAL_ALPHA:MAX_SAT_ANGLE
-    for theta=0:INTERVAL_THETA:THETA_MAX-INTERVAL_THETA
+for alpha=0:.1:pi %MAX_SAT_ANGLE
+    for theta=0:.1:2*pi %THETA_MAX-INTERVAL_THETA
+% for alpha=INTERVAL_ALPHA/2:INTERVAL_ALPHA:MAX_SAT_ANGLE
+%     for theta=0:INTERVAL_THETA:THETA_MAX-INTERVAL_THETA
         x=EARTH_RADIUS*sin(alpha)*cos(theta);
         y=EARTH_RADIUS*sin(alpha)*sin(theta);
         z=EARTH_RADIUS*cos(alpha);
+        
+
         
         %find the unit vector from earth element to satellite
         earthElementToSatVect=[SAT_X-x,SAT_Y-y,SAT_Z-z];
@@ -112,6 +115,8 @@ for alpha=INTERVAL_ALPHA/2:INTERVAL_ALPHA:MAX_SAT_ANGLE
         ACTUAL_DISTANCE_TO_SUN=((x-SUN_X)^2+(y-SUN_Y)^2+(z-SUN_Z)^2)^.5;
         
         if ACTUAL_DISTANCE_TO_SATELLITE<MAX_DISTANCE_TO_SATELLITE && ACTUAL_DISTANCE_TO_SUN<MAX_DISTANCE_TO_SUN
+            scatter3(x,y,z,'red'); % TODO DELETE IN FINAL VERSION...
+            hold on;
             %visible to both satellite and the sun.
             %This is the case where we calculate flux...
             r = ( (x-SAT_X)^2 + (y-SAT_Y)^2 + (z-SAT_Z)^2 )^.5;  %distance between area element and the satellite.
@@ -132,15 +137,28 @@ for alpha=INTERVAL_ALPHA/2:INTERVAL_ALPHA:MAX_SAT_ANGLE
                 
                 dFluxMatrix(counter,1)=dflux;
             end
+        else
+            scatter3(x,y,z,'black'); % TODO DELETE IN FINAL VERSION...
+            hold on;
         end
     end
     
-    NET_FLUX=sum(dFluxMatrix);
+
+
 end
 
 %Eliminate extra rows:
 unitVectMatrix = unitVectMatrix(1:counter,1:3);
 dFluxMatrix=sparse(dFluxMatrix);
+
+NET_FLUX=sum(dFluxMatrix);
+
+disp('***************')
+disp('NET FLUX NON-OPTIMIZED')
+disp(NET_FLUX);
+disp('***************')
+
+toc
 end
 
 %BEGIN FUNCTIONS THAT ARE OPTIMIZES AND WILL ATTEMPT TO REPLICATE THE
@@ -148,142 +166,142 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-function [dFluxMatrixOptimized,unitVectMatrixOptimized,NET_FLUX_OPTIMIZED]=conductBandSearch(START_THETA,INTERVAL_THETA,INTERVAL_ALPHA,SUN_VECTOR,SAT_VECTOR,MAX_SAT_ANGLE,MAX_SUN_ANGLE,dFluxMatrix,unitVectMatrix,R)
 % 
-counter=0; %number of rows in dflux vector and unit vector.
+% function [dFluxMatrixOptimized,unitVectMatrixOptimized,NET_FLUX_OPTIMIZED]=conductBandSearch(START_THETA,INTERVAL_THETA,INTERVAL_ALPHA,SUN_VECTOR,SAT_VECTOR,MAX_SAT_ANGLE,MAX_SUN_ANGLE,dFluxMatrix,unitVectMatrix,R)
+% % 
+% counter=0; %number of rows in dflux vector and unit vector.
+% % 
+% %iterate over the alphas...each alpha defining a new band...
+% for alpha=INTERVAL_ALPHA/2:INTERVAL_ALPHA:MAX_SAT_ANGLE
+%     
+%     %we can walk our way around each band...
+%     
+%     LEFT_HAND_THETA=START_THETA;
+%     RIGHT_HAND_THETA=START_THETA+INTERVAL_THETA; % arbitrary decision for which is where; we're just ballparking where we start.
+%     
+%     %"normalize anytime we change angle to make sure its between 0 and 2pi"
+%     %//TODO CHECK THE EDGE CASES LIKE 0 and 2pi
+%     %
+%     
+%     [LEFT_HAND_THETA]=correctAngle(LEFT_HAND_THETA);
+%     
+%     [RIGHT_HAND_THETA]=correctAngle(RIGHT_HAND_THETA);
+%     
+%     handCounter=0; % just allows me to alternate between RHS AND LHS walkers.
+%     
+%     LHS_COMPLETE=false;
+%     RHS_COMPLETE=false;
+%     
+%     while ( bugy...   (LEFT_HAND_THETA>RIGHT_HAND_THETA) && ~(RHS_COMPLETE && LHS_COMPLETE))
 % 
-%iterate over the alphas...each alpha defining a new band...
-for alpha=INTERVAL_ALPHA/2:INTERVAL_ALPHA:MAX_SAT_ANGLE
-    
-    %we can walk our way around each band...
-    
-    LEFT_HAND_THETA=START_THETA;
-    RIGHT_HAND_THETA=START_THETA+INTERVAL_THETA; % arbitrary decision for which is where; we're just ballparking where we start.
-    
-    %"normalize anytime we change angle to make sure its between 0 and 2pi"
-    %//TODO CHECK THE EDGE CASES LIKE 0 and 2pi
-    %
-    
-    [LEFT_HAND_THETA]=correctAngle(LEFT_HAND_THETA);
-    
-    [RIGHT_HAND_THETA]=correctAngle(RIGHT_HAND_THETA);
-    
-    handCounter=0; % just allows me to alternate between RHS AND LHS walkers.
-    
-    LHS_COMPLETE=false;
-    RHS_COMPLETE=false;
-    
-    while ( bugy...   (LEFT_HAND_THETA>RIGHT_HAND_THETA) && ~(RHS_COMPLETE && LHS_COMPLETE))
-
-        if mod(handCounter,2)==0
-            %before advancing, calculate the dflux
-            
-            %For each alpha, let's calculate the flux at the START_THETA
-
-            [dflux,unitElementToSatVect]=calculateDfluxForParameters(alpha,RIGHT_HAND_THETA,SUN_X,SUN_Y,SUN_Z,R)
-
-            if dflux>0 %~dflux==0
-                counter=counter+1;
-                unitVectMatrix(counter,1)=unitElementToSatVect(1,1);
-                unitVectMatrix(counter,2)=unitElementToSatVect(1,2);
-                unitVectMatrix(counter,3)=unitElementToSatVect(1,3);
-                
-                
-                dFluxMatrix(counter,1)=dflux;
-                
-            else
-                %add it to the vector....
-                RHS_COMPLETE=true;
-            end
-            
-            %advance RHS
-            RIGHT_HAND_THETA=RIGHT_HAND_THETA+THETA_INCREMENT;
-            
-            %adjust if above or below 2*pi
-            [RIGHT_HAND_THETA]=correctAngle(RIGHT_HAND_THETA);
-        else
-            
-            %before advancing, calculate the dflux
-            
-            %For each alpha, let's calculate the flux at the START_THETA
-            
-            [dflux,unitElementToSatVect]=calculateDfluxForParameters(alpha,LEFT_HAND_THETA,SUN_X,SUN_Y,SUN_Z,R)
-
-            if dflux>0 %~dflux==0
-                counter=counter+1;
-                unitVectMatrix(counter,1)=unitElementToSatVect(1,1);
-                unitVectMatrix(counter,2)=unitElementToSatVect(1,2);
-                unitVectMatrix(counter,3)=unitElementToSatVect(1,3);
-                
-                dFluxMatrix(counter,1)=dflux;
-                
-            else
-                %add it to the vector....
-                LHS_COMPLETE=true;
-            end
-            
-            %advance RHS
-            LEFT_HAND_THETA=LEFT_HAND_THETA+THETA_INCREMENT;
-            
-            %adjust if above or below 2*pi
-            [LEFT_HAND_THETA]=correctAngle(LEFT_HAND_THETA);
-            
-        end
-        handCounter=handCounter+1;
-    end
-end
-%here we should have everything done...
-NET_FLUX_OPTIMIZED=sum(dFluxMatrix);
-unitVectMatrixOptimized=unitVectMatrix;
-dFluxMatrixOptimized=dFluxMatrix;
-disp('**************');
-disp('**************');
-disp('Net Flux Using Optimized algorithm:');
-disp(NET_FLUX_OPTIMIZED);
-disp('**************');
-disp('**************');
-end
-
-function [dflux,unitElementToSatVect]=calculateDfluxForParameters(alpha,theta,SUN_X,SUN_Y,SUN_Z,R) % includes rotation matrix so we can rotate
-x=EARTH_RADIUS*sin(alpha)*cos(theta);
-y=EARTH_RADIUS*sin(alpha)*sin(theta);
-z=EARTH_RADIUS*cos(alpha);
-
-%find the unit vector from earth element to satellite
-earthElementToSatVect=[SAT_X-x,SAT_Y-y,SAT_Z-z];
-unitElementToSatVect=earthElementToSatVect/norm(earthElementToSatVect);
-Rtrans=transpose(R);
-unitElementToSatVect=unitElementToSatVect*Rtrans;
-
-ACTUAL_DISTANCE_TO_SATELLITE=((x-SAT_X)^2+(y-SAT_Y)^2+(z-SAT_Z)^2)^.5;
-ACTUAL_DISTANCE_TO_SUN=((x-SUN_X)^2+(y-SUN_Y)^2+(z-SUN_Z)^2)^.5;
-
-if ACTUAL_DISTANCE_TO_SATELLITE<MAX_DISTANCE_TO_SATELLITE && ACTUAL_DISTANCE_TO_SUN<MAX_DISTANCE_TO_SUN
-    %visible to both satellite and the sun.
-    %This is the case where we calculate flux...
-    r = ( (x-SAT_X)^2 + (y-SAT_Y)^2 + (z-SAT_Z)^2 )^.5;  %distance between area element and the satellite.
-    
-    areaEarthElement=EARTH_RADIUS^2*sin(alpha)*INTERVAL_THETA*INTERVAL_ALPHA;
-    
-    a=[x,y,z]; %vector normal to earth element.
-    b=[SUN_X-x,SUN_Y-y,SUN_Z-z]; % vector from earth element to sun
-    
-    sunAngle=acos(dot(a,b)/(norm(a)*norm(b))); %angle between a and b
-    dflux=(albedo*E_s*cos(sunAngle)+e*M_b)/(pi*r^2)*cos(alpha)*areaEarthElement; %flux calculation, value in watts/m^2
-end
-
-end
-
-function [angle]=correctAngle(angle)
-% as we're walking around angles, we correct them...so they aren't 
-    if (angle<0)
-       angle=2*pi+angle;
-    elseif angle>=2*pi
-        angle=mod(angle,2*pi);
-    end
-
-end
+%         if mod(handCounter,2)==0
+%             %before advancing, calculate the dflux
+%             
+%             %For each alpha, let's calculate the flux at the START_THETA
+% 
+%             [dflux,unitElementToSatVect]=calculateDfluxForParameters(alpha,RIGHT_HAND_THETA,SUN_X,SUN_Y,SUN_Z,R)
+% 
+%             if dflux>0 %~dflux==0
+%                 counter=counter+1;
+%                 unitVectMatrix(counter,1)=unitElementToSatVect(1,1);
+%                 unitVectMatrix(counter,2)=unitElementToSatVect(1,2);
+%                 unitVectMatrix(counter,3)=unitElementToSatVect(1,3);
+%                 
+%                 
+%                 dFluxMatrix(counter,1)=dflux;
+%                 
+%             else
+%                 %add it to the vector....
+%                 RHS_COMPLETE=true;
+%             end
+%             
+%             %advance RHS
+%             RIGHT_HAND_THETA=RIGHT_HAND_THETA+THETA_INCREMENT;
+%             
+%             %adjust if above or below 2*pi
+%             [RIGHT_HAND_THETA]=correctAngle(RIGHT_HAND_THETA);
+%         else
+%             
+%             %before advancing, calculate the dflux
+%             
+%             %For each alpha, let's calculate the flux at the START_THETA
+%             
+%             [dflux,unitElementToSatVect]=calculateDfluxForParameters(alpha,LEFT_HAND_THETA,SUN_X,SUN_Y,SUN_Z,R)
+% 
+%             if dflux>0 %~dflux==0
+%                 counter=counter+1;
+%                 unitVectMatrix(counter,1)=unitElementToSatVect(1,1);
+%                 unitVectMatrix(counter,2)=unitElementToSatVect(1,2);
+%                 unitVectMatrix(counter,3)=unitElementToSatVect(1,3);
+%                 
+%                 dFluxMatrix(counter,1)=dflux;
+%                 
+%             else
+%                 %add it to the vector....
+%                 LHS_COMPLETE=true;
+%             end
+%             
+%             %advance RHS
+%             LEFT_HAND_THETA=LEFT_HAND_THETA+THETA_INCREMENT;
+%             
+%             %adjust if above or below 2*pi
+%             [LEFT_HAND_THETA]=correctAngle(LEFT_HAND_THETA);
+%             
+%         end
+%         handCounter=handCounter+1;
+%     end
+% end
+% %here we should have everything done...
+% NET_FLUX_OPTIMIZED=sum(dFluxMatrix);
+% unitVectMatrixOptimized=unitVectMatrix;
+% dFluxMatrixOptimized=dFluxMatrix;
+% disp('**************');
+% disp('**************');
+% disp('Net Flux Using Optimized algorithm:');
+% disp(NET_FLUX_OPTIMIZED);
+% disp('**************');
+% disp('**************');
+% end
+% 
+% function [dflux,unitElementToSatVect]=calculateDfluxForParameters(alpha,theta,SUN_X,SUN_Y,SUN_Z,R) % includes rotation matrix so we can rotate
+% x=EARTH_RADIUS*sin(alpha)*cos(theta);
+% y=EARTH_RADIUS*sin(alpha)*sin(theta);
+% z=EARTH_RADIUS*cos(alph?a);
+% 
+% %find the unit vector from earth element to satellite
+% earthElementToSatVect=[SAT_X-x,SAT_Y-y,SAT_Z-z];
+% unitElementToSatVect=earthElementToSatVect/norm(earthElementToSatVect);
+% Rtrans=transpose(R);
+% unitElementToSatVect=unitElementToSatVect*Rtrans;
+% 
+% ACTUAL_DISTANCE_TO_SATELLITE=((x-SAT_X)^2+(y-SAT_Y)^2+(z-SAT_Z)^2)^.5;
+% ACTUAL_DISTANCE_TO_SUN=((x-SUN_X)^2+(y-SUN_Y)^2+(z-SUN_Z)^2)^.5;
+% 
+% if ACTUAL_DISTANCE_TO_SATELLITE<MAX_DISTANCE_TO_SATELLITE && ACTUAL_DISTANCE_TO_SUN<MAX_DISTANCE_TO_SUN
+%     %visible to both satellite and the sun.
+%     %This is the case where we calculate flux...
+%     r = ( (x-SAT_X)^2 + (y-SAT_Y)^2 + (z-SAT_Z)^2 )^.5;  %distance between area element and the satellite.
+%     
+%     areaEarthElement=EARTH_RADIUS^2*sin(alpha)*INTERVAL_THETA*INTERVAL_ALPHA;
+%     
+%     a=[x,y,z]; %vector normal to earth element.
+%     b=[SUN_X-x,SUN_Y-y,SUN_Z-z]; % vector from earth element to sun
+%     
+%     sunAngle=acos(dot(a,b)/(norm(a)*norm(b))); %angle between a and b
+%     dflux=(albedo*E_s*cos(sunAngle)+e*M_b)/(pi*r^2)*cos(alpha)*areaEarthElement; %flux calculation, value in watts/m^2
+% end
+% 
+% end
+% 
+% function [angle]=correctAngle(angle)
+% % as we're walking around angles, we correct them...so they aren't 
+%     if (angle<0)
+%        angle=2*pi+angle;
+%     elseif angle>=2*pi
+%         angle=mod(angle,2*pi);
+%     end
+% 
+% end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
---
+++ Satellite_Simulation_1/scatter_error_test.m
@@ -0,0 +1,58 @@
+%This program attempts to quantitatively show relationships between
+%timesteps and error; I haven't thought of an analytical way to do this (and honestly haven't thought hard about it, so here's a few options)
+
+x=1;
+y=2.9175e+03;
+scatter(x,y);
+hold on;
+
+x=5;
+y=1.7505e+03
+scatter(x,y);
+hold on;
+
+x=10;
+y=1.6047e+03;
+scatter(x,y);
+hold on;
+
+x=20;
+y=1.5317e+03;
+scatter(x,y);
+hold on;
+
+x=30;
+y=1.5074e+03;
+scatter(x,y);
+hold on;
+
+x=40;
+y=1.4952e+03;
+scatter(x,y);
+hold on;
+
+x=50;
+y=1.4879e+03;
+scatter(x,y);
+hold on;
+
+x=60;
+y=1.4831e+03;
+scatter(x,y);
+hold on;
+% 
+% 1 result:     2.9175e+03
+% 
+% 5 result:     1.7505e+03
+% 
+% 10 result:   1.6047e+03
+% 
+% 20 result:    1.5317e+03
+% 
+% 30 result:    1.5074e+03
+% 
+% 40 result:    1.4952e+03
+% 
+% 50 result:    1.4879e+03
+% 
+% 60  result:   1.4831e+03
